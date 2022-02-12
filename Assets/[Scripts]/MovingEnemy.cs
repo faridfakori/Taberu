cdaa4320667995326a3027be93fa0aa4f7856d45
Dogyeom Lee <dlehrua0712@gmail.com>
2022-02-11 19:39:58 -0500

    add moving enemy

--- Assets/[Scripts]/MovingEnemy.cs
+++ Assets/[Scripts]/MovingEnemy.cs
@@ -6,25 +6,43 @@ public class MovingEnemy : MonoBehaviour
 {
     Rigidbody2D rigid;
     public int nextmove;
+    SpriteRenderer rend;
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-
-        Think();
-
-        Invoke("Think", 5);
+        rend = GetComponent<SpriteRenderer>();
+        Invoke("Think", 3);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(nextmove, rigid.velocity.y);
+     
+        if(nextmove == -1)
+        {
+            rend.flipX = false;
+        }
+        else if (nextmove == 1)
+        {
+            rend.flipX = true;
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            rigid.velocity = new Vector2(nextmove, rigid.velocity.y);
+
+        }
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
     }
     void Think()
     {
         nextmove = Random.Range(-1,2);
-
-        Invoke("Think", 5);
+        Invoke("Think", 3);
     }
 }
